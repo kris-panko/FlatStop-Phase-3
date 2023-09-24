250e4d31b01555341e624b12e650721154f741bb
Sergio Heredia <sergioheredia1996@gmail.com>
2023-09-23 21:16:00 -0700

    added game class methods

--- lib/seed.py
+++ lib/seed.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3 
 
 from models.shopper import Shopper
+from models.game import Game
 
 def fill_database():
     # May have to delete drop_table later if we want information in database to persist
     Shopper.drop_table()
     Shopper.create_table()
+    Game.drop_table()
+    Game.create_table()
+
 
     Shopper.create("Sergio", "password1", 27,)
+    Game.create("Call of Duty", 20.00, "M")
+    Game.create("Call of Duty", 20.00, "M")
+    Game.create("Call of Duty", 20.00, "M")
+    Game.create("Call of Duty", 20.00, "M")
+    Game.create("Call of Duty", 20.00, "M")
+    Game.create("Elden Ring", 60.00, "M")
+    Game.create("Elden Ring", 60.00, "M")
+    Game.create("Ratchet and Clank", 30.00, "T")
 
 fill_database()
