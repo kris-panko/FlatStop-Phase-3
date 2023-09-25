c56926ca1544943ad39f23d33765e9e9ec772399
Sergio Heredia <sergioheredia1996@gmail.com>
2023-09-24 17:28:40 -0700

    Added view account

--- lib/helper_functions/main_menu_functions.py
+++ lib/helper_functions/main_menu_functions.py
@@ -1,6 +1,7 @@
 from helper_functions.browse_store_functions import *
 from helper_functions.buy_checkout_functions import *
 from helper_functions.sell_functions import *
+from helper_functions.view_account import *
 
 def main_menu(shopper):
     def prompt2():
@@ -9,7 +10,8 @@ def main_menu(shopper):
         print("2. Buy Items")
         print("3. Sell Items")
         print("4. Chat")
-        print("5. Leave Store")
+        print("5. View your account")
+        print("6. Leave Store")
 
     while True:
         prompt2()
@@ -19,10 +21,12 @@ def main_menu(shopper):
         elif choice_2 == "2":
             buy_from_store()
         elif choice_2 == "3":
-            sell_game(shopper) #PASS IN A GAME ID**
+            sell_game(shopper) 
         elif choice_2 == "4":
             print("Will show chatting prompt and will prompt questions for store attendee?\n")
         elif choice_2 == "5":
+            get_account(shopper)
+        elif choice_2 == "6":
             exit_program()
         else:
             print(f"{choice_2} is not a valid input. Please enter a number 1-5.\n")
---
+++ lib/helper_functions/view_account.py
@@ -0,0 +1,33 @@
+from models.shopper import Shopper
+
+def get_account(shopper):
+    curr_shopper = Shopper.find_by_username(shopper.user_name)
+    print("\nHere are your account details:\n")
+    print("Username: " + curr_shopper.user_name)
+    print("Password: " + curr_shopper.password)
+    print("Age: ", curr_shopper.age)
+    while True:
+        print("\n1. Would you like to update your account?")
+        print("2. Would you like to delete your account?")
+        print("3. Go back to main menu")
+        choice = input("> ")
+        if choice == "1":
+            print("\n1. Update username")
+            print("2. Update password")
+            print("3. Update age")
+            get_account(shopper)
+        elif choice == "2":
+            print("\nAre you sure you want to delete your account?")
+            print("We'd hate to see you go....")
+            choice1 = input("[y/N] ")
+            if choice1 == "y":
+                print("\nFine, good riddance...")
+            elif choice == "N":
+                get_account(shopper)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice, please enter a number 1-3")
+
+
+            
