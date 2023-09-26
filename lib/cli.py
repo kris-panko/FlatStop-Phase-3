0d10ea5d5a1cbae437022c039ae2b43d0decdc37
Kim Love <kimberlymlove15@gmail.com>
2023-09-26 01:52:29 -0400

    Adds password to user and additional login and create account functionality

--- lib/cli.py
+++ lib/cli.py
@@ -1,27 +1,48 @@
 #!/usr/bin/env python3 
-
 from helpers import *
 
 
 def main():
-    while True:
-        login_or_create_account()
+    #cur_user = login_or_create_account() - if we want to use name of user
+    print("Welcome to FlatStop!")
+    login_or_create_account()
+    buy_sell_menu()
 
 
 def login_or_create_account():
     print("Do you have an account?")
-    choice = input("[y/N] ")
+    choice = input("[y/N]> ")
     if choice == "y":
+        # name = login() - if we want to use name of user
         login()
     elif choice == "N":
+        # name = create_account() - if we want to use name of user
         create_account()
     else:
-        print("Invalid choice")
+        print("Invalid choice, please enter y for YES or N for no.")
+        login_or_create_account()
+    
+    # return name - if we want to use name of user
 
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+def buy_sell_menu():
+    print("How can we help you today?")
+    print("Please select one of the following options: ")
+    print("0. Log out")
+    print("1. Buy a video game or console.")
+    print("2. Sell a video game or console.")
+    
+    choice = input("> ")
+    if choice == "0":
+        print("Okay please come back soon!")
+        print("Logging out....")
+        exit_program()
+    elif choice == "1":
+        buy_game_or_console()
+    elif choice =="2":
+        sell_game_or_console()
+    else:
+        print("Invalid choice, please enter a valid number: 0, 1, 2")
+        buy_sell_menu()
 
 
 if __name__ == "__main__":
--- lib/models/shopper.py
+++ lib/models/shopper.py
@@ -2,10 +2,11 @@ from models.__init__ import CURSOR, CONN
 
 class Shopper:
     all = {}
-    def __init__(self, user_name, age, id=None):
+    def __init__(self, user_name, password, age, id=None):
         self.user_name = user_name
         self.age = age
         self.id = id
+        self.password = password
     @property
     def age(self):
         return self._age
@@ -24,6 +25,13 @@ class Shopper:
             self._user_name = user_name
         else:
             print("Please try entering username again: Must be a string and greater than 3 characters long")
+    @property
+    def password(self):
+        return self._password
+    @password.setter
+    def password(self, password):
+        if (isinstance(password, str) and len(password) >= 8):
+            self._password = password
 
     @classmethod
     def create_table(cls):
@@ -31,6 +39,7 @@ class Shopper:
             CREATE TABLE IF NOT EXISTS shoppers (
             id INTEGER PRIMARY KEY,
             user_name TEXT,
+            password TEXT,
             age INTEGER);
         """
         CURSOR.execute(sql)
@@ -45,17 +54,17 @@ class Shopper:
         CONN.commit()
 
     @classmethod
-    def create(cls, user_name, age):
-        shopper = cls(user_name, age)
+    def create(cls, user_name, password, age):
+        shopper = cls(user_name, password, age)
         shopper.save()
         return shopper
     
     def save(self):
         sql = """
-            INSERT INTO shoppers (user_name, age)
-            VALUES (?, ?)
+            INSERT INTO shoppers (user_name, password, age)
+            VALUES (?, ?, ?)
         """
-        CURSOR.execute(sql, (self.user_name, self.age))
+        CURSOR.execute(sql, (self.user_name, self.password, self.age))
         CONN.commit()
 
         self.id = CURSOR.lastrowid
@@ -64,10 +73,11 @@ class Shopper:
     def instance_from_db(cls, row):
         shopper = cls.all.get(row[0])
         if shopper:
-            shopper.name = row[1]
-            shopper.age = row[2]
+            shopper.user_name = row[1]
+            shopper.password = row[2]
+            shopper.age = row[3]
         else:
-            shopper = cls(row[1], row[2])
+            shopper = cls(row[1], row[2], row[3])
             shopper.id = row[0]
             cls.all[shopper.id] = shopper
         return shopper
@@ -81,3 +91,17 @@ class Shopper:
         """
         row = CURSOR.execute(sql, (user_name,)).fetchone()
         return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def does_username_exist(cls, user_name):
+        sql = """
+            SELECT * 
+            FROM shoppers
+            WHERE user_name = ?
+        """
+        row = CURSOR.execute(sql, (user_name,)).fetchone()
+
+        if row == None:
+            return False
+        else:
+            return True
