2b6146cdd7faf109d01e4304f5bb8c8080369a08
Kim Love <kimberlymlove15@gmail.com>
2023-09-26 12:03:11 -0400

    Merges Kim's code into main

--- lib/helpers.py
+++ lib/helpers.py
@@ -6,26 +6,72 @@ def create_account():
     choice = input("[y/N]> ").lower()
     if choice == "y":
         username = input("Please create a username: ")
+        exists = Shopper.does_username_exist(username)
+
+        if exists == True:
+            print("This username is already taken, please enter a different one.")
+            create_account()
+
         age = input("Please enter age: ")
+        print("Password must be 8 characters or longer.")
+        password = input("Please enter a password: ")
 
         try:
-            shopper = Shopper.create(username, int(age))
+            shopper = Shopper.create(username, password, int(age))
             print(f"Hi {shopper.user_name}, your account was created successfully!")
+            # return shopper.user_name - if we want to use shopper name later
         except Exception as exc:
-            print("Sorry there was an error creating your account. Please try again later: ", exc)
+            print("There was an error creating your account. Please try again.")
+            print("Error:", exc)
+            create_account()
 
     else:
         print("Okay maybe next time!")
         exit_program()
+
 def login():
+    login_attempts = 0
+    max_attempts = 3
+
     username = input("Please enter your username: ")
-    try:
-        print("Logging in....")
-        #changed shopper function name here
-        shopper = Shopper.find_by_username(username)
-        print(f"Hi {shopper.user_name} welcome back! ")
-    except Exception as exc:
-        print("Sorry no such account exists, please try again")
+    shopper = Shopper.find_by_username(username)
+
+    if shopper:
+        while login_attempts < max_attempts:
+            password = input("Enter password: ")
+            if shopper.password == password:
+                print("Logging in....")
+                #changed shopper function name here
+                shopper = Shopper.find_by_username(username)
+                print(f"Hi {shopper.user_name}, welcome back! ")
+                break
+                # return shopper.user_name - if we want to use shopper name later
+            else:
+                login_attempts += 1
+                print(f"Login failed, incorrect password. Please try again. attempt: {login_attempts}. Max attempts: {max_attempts}.")
+        if login_attempts == max_attempts:        
+            print("Maximum login attempts reached. Your account has been locked.")
+            exit_program()
+    else:
+        print("Sorry no such username exists!")
+        print("Would you like to try to login in again or create a new account?")
+        print("0. Exit program")
+        print("1. Login again")
+        print("2. Create an account")
+        login_menu()
+
+def login_menu():
+    choice = input("> ")
+    if choice == "0":
+        print("Okay please come back soon!")
+        exit_program()
+    elif choice == "1":
+        login()
+    elif choice =="2":
+        create_account()
+    else:
+        print("Invalid choice, please enter a valid number: 0, 1, 2")
+        login_menu()
 
 def exit_program():
     print("Goodbye!")
--- lib/models/shopper.py
+++ lib/models/shopper.py
@@ -3,10 +3,11 @@ from models.__init__ import CURSOR, CONN
 class Shopper:
    #got rid of the all set because single source of truth could be database
     
-    def __init__(self, user_name, age, id=None):
+    def __init__(self, user_name, password, age, id=None):
         self.user_name = user_name
         self.age = age
         self.id = id
+        self.password = password
     @property
     def age(self):
         return self._age
@@ -25,6 +26,13 @@ class Shopper:
             self._user_name = user_name
         else:
             print("Please try entering username again: Must be a string and greater than 3 characters long")
+    @property
+    def password(self):
+        return self._password
+    @password.setter
+    def password(self, password):
+        if (isinstance(password, str) and len(password) >= 8):
+            self._password = password
 
     @classmethod
     def create_table(cls):
@@ -32,6 +40,7 @@ class Shopper:
             CREATE TABLE IF NOT EXISTS shoppers (
             id INTEGER PRIMARY KEY,
             user_name TEXT,
+            password TEXT,
             age INTEGER);
         """
         CURSOR.execute(sql)
@@ -49,15 +58,15 @@ class Shopper:
 #instantiates, adds to database, and then returns it
 #no all necessary becasue database is SSoT
     @classmethod
-    def create(cls, user_name, age):
+    def create(cls, user_name, password, age):
         #creates the table the first time, then won't do anything else once created
         cls.create_table()
-        shopper = cls(user_name, age)
+        shopper = cls(user_name, password, age)
         sql = """
-            INSERT INTO shoppers (user_name, age)
-            VALUES (?, ?)
+            INSERT INTO shoppers (user_name, password, age)
+            VALUES (?, ?, ?)
         """
-        CURSOR.execute(sql, (shopper.user_name, shopper.age))
+        CURSOR.execute(sql, (shopper.user_name, shopper.password, shopper.age))
         CONN.commit()
 
         shopper.id = CURSOR.lastrowid
@@ -72,8 +81,8 @@ class Shopper:
         #destructured tuple-rows into variables
         #id, user_name, age
         #for this row, make a shopper out of it
-        id, user_name, age = row
-        return cls(user_name, age, id)
+        id, user_name, password, age = row
+        return cls(user_name, password, age, id)
 
         # shopper = cls.all.get(row[0])
         # if shopper:
@@ -103,3 +112,17 @@ class Shopper:
         """
         row = CURSOR.execute(sql, (user_name,)).fetchone()
         return cls.db_to_object(row) if row else None
+    
+    @classmethod
+    def does_username_exist(cls, user_name):
+        sql = """
+            SELECT * 
+            FROM shoppers
+            WHERE user_name = ?
+        """
+        row = CURSOR.execute(sql, (user_name,)).fetchone()
+
+        if row == None:
+            return False
+        else:
+            return True
